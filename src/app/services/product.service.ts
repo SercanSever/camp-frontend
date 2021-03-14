ce3a43c099fb3b41c0c2f3b35933f749794baf2b
Sercan Sever <sercan.sever16@gmail.com>
2021-03-14 21:43:18 +0300

    14.03.21

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ProductListResponseModel } from 'src/app/models/productListResponseModel';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { ListResponseModel } from '../models/ListResponseModel';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   apiUrl: string = 'https://localhost:44343/api/Products/getAll';
+
   constructor(private httpClient: HttpClient) {}
 
-  getProducts(): Observable<ProductListResponseModel> {
-    return this.httpClient.get<ProductListResponseModel>(this.apiUrl);
+  getProducts(): Observable<ListResponseModel<Product>> {
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
   }
 }
