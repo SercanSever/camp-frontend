4030ec8f524a4f7ffd8c6c58f11e6ce50a9a5272
Sercan Sever <sercan.sever16@gmail.com>
2021-03-15 02:58:59 +0300

    15.03.21

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -19,7 +19,7 @@ export class ProductService {
   }
 
   getProductsByCategory(categoryId: number): Observable<ListResponseModel<Product>> {
-    let newPath = this.apiUrl + "Products/getbycategory?categoryId" + categoryId
+    let newPath = this.apiUrl + "Products/getbycategory?categoryId=" + categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 }
