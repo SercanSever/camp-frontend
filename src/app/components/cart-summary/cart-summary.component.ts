80060ac9a7a78c73aa30253198685fe2b7f5f1ca
Sercan Sever <sercan.sever16@gmail.com>
2021-03-19 23:47:25 +0300

    19.03.21

--- src/app/components/cart-summary/cart-summary.component.ts
+++ src/app/components/cart-summary/cart-summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/models/cartItem';
 
 @Component({
   selector: 'app-cart-summary',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-summary.component.css']
 })
 export class CartSummaryComponent implements OnInit {
+  cartItems: CartItem[] = [];
 
   constructor() { }
 
