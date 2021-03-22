48b70b06219e3d41e8642d553a1eb5b1e03e2f0e
Sercan Sever <sercan.sever16@gmail.com>
2021-03-23 01:01:01 +0300

    23.03.21

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -31,8 +31,13 @@ export class ProductAddComponent implements OnInit {
     if (this.productAddForm.valid) {
       let productModel = Object.assign({}, this.productAddForm.value)
       this.productService.add(productModel).subscribe(response => {
-        console.log(response);
         this.toastrService.success(response.message, "Başarılı");
+      }, responseError => {
+        if (responseError.error.Errors.length > 0) {
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage, "Doğrulama Hatası");
+          }
+        }
       });
     } else {
       this.toastrService.error("Boş Alanları Doldurunuz", "Dikkat");
