b3b8f842b096787075a308d343bd92fca9523a16
Sercan Sever <sercan.sever16@gmail.com>
2021-03-22 23:17:12 +0300

    22.03.21

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms"
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
@@ -10,7 +12,8 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private productService: ProductService,
+    private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -24,6 +27,15 @@ export class ProductAddComponent implements OnInit {
       unitsInStock: ["", Validators.required]
     })
   }
-
-
+  add() {
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response => {
+        console.log(response);
+        this.toastrService.success(response.message, "Başarılı");
+      });
+    } else {
+      this.toastrService.error("Boş Alanları Doldurunuz", "Dikkat");
+    }
+  }
 }
